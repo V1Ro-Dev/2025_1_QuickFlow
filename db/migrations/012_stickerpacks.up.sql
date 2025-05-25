0bf9c490842e949e21e487e3fab05e50021830c2
Nok1o <nikitamogilin39@gmail.com>
2025-05-25 16:54:36 +0300

    migrations.

--- db/migrations/012_stickerpacks.up.sql
+++ db/migrations/012_stickerpacks.up.sql
@@ -1,2 +1,14 @@
-drop table if exists sticker_pack cascade;
-drop table if exists sticker cascade;
+create table if not exists sticker_pack(
+                                           id uuid primary key default gen_random_uuid(),
+                                           name TEXT NOT NULL,
+                                           creator_id uuid references "user"(id) on delete cascade,
+                                           created_at TIMESTAMP DEFAULT NOW(),
+                                           updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS sticker (
+                                       id UUID primary key default gen_random_uuid(),
+                                       sticker_pack_id UUID REFERENCES sticker_pack(id) ON DELETE CASCADE,
+                                       sticker_url text not null,
+                                       created_at TIMESTAMP DEFAULT NOW()
+);
