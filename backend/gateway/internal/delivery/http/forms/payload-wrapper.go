d1c92fab52cae087f129e25bf6dac9ef8fda7cf9
Nok1o <nikitamogilin39@gmail.com>
2025-05-27 11:16:57 +0300

    payload.

--- backend/gateway/internal/delivery/http/forms/payload-wrapper.go
+++ backend/gateway/internal/delivery/http/forms/payload-wrapper.go
@@ -13,9 +13,40 @@ func (pw *PayloadWrapper[T]) Unwrap() T {
 }
 
 func (pw *PayloadWrapper[T]) MarshalJSON() ([]byte, error) {
-	return json.Marshal(pw.Payload)
+	// Строим объект JSON с полем "payload"
+	return json.Marshal(struct {
+		Payload T `json:"payload"`
+	}{
+		Payload: pw.Payload,
+	})
 }
 
-func (pw *PayloadWrapper[T]) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, &pw.Payload)
-}
+//func (pw *PayloadWrapper[T]) MarshalEasyJSON(w *jwriter.Writer) {
+//	// Начинаем сериализацию
+//	w.RawByte('{')
+//	w.RawString("\"payload\":")
+//	// Сериализуем поле Payload с использованием easyjson
+//	// Для поля Payload мы предполагаем, что тип T уже поддерживает easyjson
+//	easyjson.MarshalToWriter(pw.Payload, w)
+//	// Закрываем объект JSON
+//	w.RawByte('}')
+//}
+//
+//// UnmarshalEasyJSON десериализует PayloadWrapper с использованием easyjson
+//func (pw *PayloadWrapper[T]) UnmarshalEasyJSON(l *jlexer.Lexer) {
+//	// Начинаем десериализацию
+//	l.Delim('{')
+//	for !l.IsDelim('}') {
+//		key := l.UnsafeFieldName(false)
+//		l.WantColon()
+//		switch key {
+//		case "payload":
+//			// Десериализуем поле Payload с использованием easyjson
+//			easyjson.Unmarshal(l, &pw.Payload)
+//		default:
+//			l.SkipRecursive()
+//		}
+//		l.WantComma()
+//	}
+//	l.Delim('}')
+//}
